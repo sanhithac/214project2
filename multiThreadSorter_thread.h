4095ce5f0f2e44ff07b2630594b184c973bed5b3
sanhithac <sanhitha.c@gmail.com>
2018-11-19 10:56:52 -0500

    Update multiThreadSorter_thread.h

--- multiThreadSorter_thread.h
+++ multiThreadSorter_thread.h
@@ -1,1 +1,30 @@
+#include <stdlib.h>
 
+typedef struct Node{
+  char *data;
+  char *row;
+  struct Node *next;
+}Node;
+
+typedef struct pidNode{
+  pid_t *pid;
+  struct pidNode *next;
+}pidNode;
+
+int sorter(FILE *fp, char *colName, DIR *dir);
+Node* readfile(int colInd, FILE *fp);
+char *remove_leading_spaces(char* str);
+int columnNum(char *row, char *col);
+char *tokenizer(int col, char *line);
+void FrontBackSplit (Node* source, Node** frontRef, Node** backRef);
+void MergeSort (Node** headRef);
+Node* SortedMerge( Node* a, Node* b);
+void print( Node* a);
+void Finish(Node* head);
+
+
+void directChild(DIR* dir, pidNode * head, int processCounter);
+int isDirectory(char d_name[]);
+int isCSV(char d_name[]);
+pidNode* childpid(pidNode * head, pid_t pid);
+void printVals(pid_t pid, pidNode *head, int counter);
